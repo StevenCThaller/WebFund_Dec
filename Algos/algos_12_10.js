b2edbc6ccfc6655c5ecc74a06f30201858386b7b
Cody Thaller <stevencthaller@gmail.com>
2020-12-14 08:36:46 -0800

    Added algo prompt 12/14

--- Algos/algos_12_10.js
+++ Algos/algos_12_10.js
@@ -1,5 +1,5 @@
 // EXERCISE 1
-function a(arr){
+function a(arr){ // this function swaps the first and last elements of an array
     var b = arr.length;
     var temp = arr[b-1];
 
@@ -8,14 +8,22 @@ function a(arr){
     return arr;
 }
 
-console.log(a([1,2,3]));
-console.log(a([2,4]));
+console.log(a([1,2,3])); // output: [3,2,1]
+console.log(a([2,4])); // output: [4,2]
+
+
+
+
+
+
+
 
 
 // EXERCISE 2
-function b(arr){
+// ORIGINAL:
+function bWrong(arr) {
     var x = arr.length;
-    
+
     for(var i = 0; i < x/2; i++) {
         var temp = arr[x-i-1];
         arr[i] = arr[x-i-1];
@@ -23,5 +31,19 @@ function b(arr){
     }
 }
 
-console.log(b([1,2,3]));
-console.log(b([1,2,3,4,5]));
+
+// FIXED CODE
+function b(arr){ // this function reverses an entire array
+    var x = arr.length;
+    
+    for(var i = 0; i < x/2; i++) {
+        var temp = arr[i]; // UPDATED: changing from arr[x-i-1] to arr[i]
+        arr[i] = arr[x-i-1];
+        arr[x-i-1] = temp;
+    }
+
+    return arr; // UPDATED: return the actual array
+}
+
+console.log(b([1,2,3])); // expected output: 3,2,1
+console.log(b([1,2,3,4,5])); // expected output: 5,4,3,2,1
---
+++ Algos/algos_12_14.js
@@ -0,0 +1,30 @@
+// Algorithm Practice - Arrays
+// It's now time to practice writing your own algorithms using arrays!  
+// Create an algorithm for each of the challenges below.  Make sure that 
+// you run your code to ensure your output is matching the provided expected 
+// output.
+
+// Using the given array for testing:
+
+var testArr = [6,3,5,1,2,4]
+
+// WARM-UP: Print all the values in the array using a for loop
+
+
+// CHALLENGE 1: Print Values and Sum
+
+// Print each array value and the sum so far
+// The expected output will be: 
+// Num 6, Sum 6
+// Num 3, Sum 9
+// Num 5, Sum 14
+// Num 1, Sum 15
+// Num 2, Sum 17
+// Num 4, Sum 21
+
+
+// CHALLENGE 2: Value * Position
+
+// Multiply each value in the array by its array position (index)
+// The expected output will be:
+// [0,3,10,3,8,20]
